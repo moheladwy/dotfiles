bd01cdaaac9fd3e563166951e0f207805f341d96
moheladwy <mohamed.h.eladwy@gmail.com>
2024-04-27 21:55:17 +0300

    add new script for installing Lenovo Legion 5 Kernel Modules for Arch Linux

--- 01-setup-arch.sh
+++ 01-setup-arch.sh
@@ -15,64 +15,64 @@ echo -e "[${Red}*${Whi}] Updating system.."
 sudo pacman -Syu
 sudo pacamn -Fyu
 
-echo ${Sperator}
+echo "$Sperator"
 
 # Install git
 echo -e "[${Red}+${Whi}] Installing git if not already installed.."
 sudo pacman -S --noconfirm --needed git
 
-echo ${Sperator}
+echo "$Sperator"
 
 # Install aura AUR helper
 echo -e "[${Red}+${Whi}] Installing yay AUR helper.."
-cd $HOME
+cd "$HOME" || return
 # Check if ~/src exists
-if [! -d ~/src]; then
+if [ ! -d ~/src ]; then
 	mkdir ~/src # Create src directory if it doesn't exist
 fi
-cd ~/src
+cd ~/src || return
 git clone https://aur.archlinux.org/yay.git yay         # Clone yay from the AUR to ~/src/yay
-cd yay                                                  # Change to yay directory
+cd yay || return                                        # Change to yay directory
 makepkg -sfci --noconfirm --needed                      # Build and install yay
 echo -e "[${Red}+${Whi}] yay version: $(yay --version)" # Print yay version to terminal.
 
-echo ${Sperator}
+echo "$Sperator"
 
 # Install Flatpak
 echo -e "[${Red}+${Whi}] Installing flatpak if not already installed.."
 sudo pacman -S --noconfirm --needed flatpak
 
-echo ${Sperator}
+echo "$Sperator"
 
 # Install pkgs
 echo -e "[${Red}+${Whi}] Installing pacman repo packages.."
-for repo_pkg in $(cat ~/dotfiles/pkgs/pacman_pkgs); do
-	sudo pacman -S --noconfirm --needed $repo_pkg
+for repo_pkg in "$(cat ~/dotfiles/pkgs/pacman_pkgs)"; do
+	sudo pacman -S --noconfirm --needed "$repo_pkg"
 done
 
-echo ${Sperator}
+echo "$Sperator"
 
 # Install pkgs from the AUR
-echo -e "[${Cya}+${Whi}] Installing AUR packages using yay.."
-for aur_pkg in $(cat ~/dotfiles/pkgs/yay_pkgs); do
-	sudo yay -Acax --noconfirm $aur_pkg
+echo -e "[$Cya+$Whi] Installing AUR packages using yay.."
+for aur_pkg in "$(cat ~/dotfiles/pkgs/yay_pkgs)"; do
+	sudo yay -Acax --noconfirm "$aur_pkg"
 done
 
-echo ${Sperator}
+echo "$Sperator"
 
 # Install Flatpak apps
-echo -e "[${Red}*${Whi}] Installing Flatpak apps.."
-for flatpak_app in $(cat ~/dotfiles/pkgs/flatpak_pkgs); do
-	flatpak install flathub $flatpak_app -y
+echo -e "[${Red}*{$Whi}] Installing Flatpak apps.."
+for flatpak_app in "$(cat ~/dotfiles/pkgs/flatpak_pkgs)"; do
+	flatpak install flathub "$flatpak_app" -y
 done
 
-echo ${Sperator}
+echo "$Sperator"
 
 # Change shell for eladwy
 echo -e "[${Red}*${Whi}] Changing shell for the user to zsh instead of bash"
-sudo usermod --shell /usr/bin/zsh $USER
+sudo usermod --shell /usr/bin/zsh "$USER"
 
-echo ${Sperator}
+echo "$Sperator"
 
 # Clone zsh-autosuggestions and zsh-syntax-highlighting plugins
 echo -e "[${Red}*${Whi}] Cloning zsh-autosuggestions plugin.."
@@ -80,12 +80,23 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/p
 echo -e "[${Red}*${Whi}] Cloning zsh-syntax-highlighting plugin.."
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
-echo ${Sperator}
+echo "$Sperator"
+
+echo -e "[${Red}*${Whi}] Are You using Lenovo Legion 5 Laptop [y/n](any other answer except y will be considered as no)?  "
+read -r answer
+
+if [ "$answer" != "${answer#[Yy]}" ]; then
+	# Install Lenovo Legion 5 kernel modules for Arch Linux.
+	echo -e "[${Red}*${Whi}] Installing Lenovo Legion 5 kernel modules for Arch Linux.."
+	cd ~/dotfiles || return
+	chmod +x ~/dotfiles/02-setup-lenovo-legion5-modules.sh
+	./02-setup-lenovo-legion5-modules.sh
+fi
 
 # Goto dotfiles.
 # Running next script in dotfiles directory.."
-cd ~/dotfiles
-echo -e "[${Red}*${Whi}] Changing permissions for 02-config.sh script and making it executable"
+cd ~/dotfiles || return
+echo -e "[${Red}*${Whi}] Changing permissions for 03-configs.sh script and making it executable"
 chmod +x ~/dotfiles/02-config.sh
-echo -e "[${Red}*${Whi}] Running 02-config.sh script.."
-./02-config.sh
+echo -e "[${Red}*${Whi}] Running 03-configs.sh script.."
+./03-configs.sh
---
+++ 02-setup-lenovo-legion5-modules.sh
@@ -0,0 +1,62 @@
+Red='\e[0;31m'
+Gre='\e[0;32m'
+Cya='\e[0;36m'
+Whi='\e[0;37m'
+Sperator="======================================================================================="
+
+# setup Lenovo Legion 5 - arch Linux modules.
+
+echo -e "[${Red}*${Whi}] Installig and setup the Lenovo Legion 5 modules for arch linux ..."
+
+echo -e "$Sperator"
+
+echo -e "[${Red}*${Whi}] Installig some dependencies packages for the setup!"
+sudo pacman -S --needed linux-headers base-devel lm_sensors git dmidecode python-pyqt5 python-yaml python-argcomplete python-darkdetect
+# Install the following for installation with DKMS
+sudo pacman -S --needed dkms openssl mokutil
+
+echo -e "[${Red}*${Whi}] making directory src in home if not exists..."
+
+mkdir -p ~/src
+cd ~/src || return
+
+git clone https://github.com/johnfanv2/LenovoLegionLinux.git
+cd LenovoLegionLinux/kernel_module || return
+
+make
+sudo make reloadmodule
+
+make
+sudo make dkms
+
+output=$(sudo dmesg || grep -i "legion_laptop loaded for this device")
+good_output="legion_laptop loaded for this device"
+
+if [ "$output" = "$good_output" ]; then
+	echo -e "[${Gre}*${Whi}] the modules installed successfully!"
+else
+	echo -e "[${Red}*${Whi}] the modules couldn't be installed successfully!"
+	exit 1
+fi
+
+echo "export PATH=$PATH:$HOME/src/LenovoLegionLinux/python/legion_linux/legion_linux" >>~/.zshrc
+
+echo -e "[${Red}*${Whi}] Adding Desktop Entry for the GUI Application..."
+touch ~/.local/share/applications/LenovoLegionLinux.desktop
+FileContent="
+[Desktop Entry]
+Name=Lenovo Legion5 Linux GUI
+Exec=sudo ~/src/LenovoLegionLinux/python/legion_linux/legion_linux/legion_gui.py
+Version=1.0
+Type=Application
+Categories=System;
+Terminal=true
+Icon=~/src/LenovoLegionLinux/python/legion_linux/legion_linux/legion_logo_dark.png
+Comment=Control Lenovo Legion 5 Laptops in Arch Linux.
+StartupNotify=false
+"
+echo "$FileContent" >>~/.local/share/applications/LenovoLegionLinux.desktop
+
+echo -e "[${Red}*${Whi}] to use the GUI Controller run: sudo ~/src/LenovoLegionLinux/python/legion_linux/legion_linux/legion_gui.py"
+echo -e "[${Red}*${Whi} or from the desktop menu item created...]"
+echo -e "[${Gre}*${Whi}] Finished the Lenovo Legion 5 Modules installation for Arch Linux System, for more information visit the github repo for this modules: https://github.com/johnfanv2/LenovoLegionLinux"
