9db0ab886e72278da8042c1416e1f3012eb8cc4b
moheladwy <mohamed.h.eladwy@gmail.com>
2025-09-21 15:14:51 +0300

    remove symbolic link

--- scripts/update_dotfiles.sh
+++ scripts/update_dotfiles.sh
@@ -11,9 +11,9 @@ COMMIT_MESSAGE="updated the last config files on $(date '+%Y-%m-%d %h-%m')"
 mkdir -p "$DEST_DIR"
 
 # Copy files from source to destination, preserving the directory structure
-cp -r -f "$SOURCE_DIR/hypr" "$DEST_DIR/hypr"
-cp -r -f "$SOURCE_DIR/ml4w" "$DEST_DIR/ml4w"
-cp -r -f "$SOURCE_DIR/waybar" "$DEST_DIR/waybar"
+cp -r -f "$SOURCE_DIR/hypr" "$DEST_DIR/"
+cp -r -f "$SOURCE_DIR/ml4w" "$DEST_DIR/"
+cp -r -f "$SOURCE_DIR/waybar" "$DEST_DIR/"
 
 # ASK Whether to commit changes or not.
 read -r -p "Do you want to commit the changes? (y/n): " user_input
